11436f870112ddce68196863a645b8f8dfa932cc
CrafterLP2007 <crafterlp2007@gmail.com>
2023-06-05 15:05:30 +0200

    Fixed bugs & Added new cool things :)

--- skillmanager.cjs
+++ skillmanager.cjs
@@ -1,43 +1,79 @@
 const fs = require("fs");
 const path = require("path");
 
-const skillPath = "./skills";
+const skillPath = "./skills/";
 const command = "test";
 
-fs.readdir(skillPath, (err, files) => {
-    if (err) {
-        console.log("Error while reading skillpath: ", err);
-        return;
-    }
-
-    files.forEach(file => {
-        if (path.extname(file) === ".json") {
-            const jsonPath = path.join(skillPath, file);
-
-            fs.readFile(jsonPath, "utf8", (err, data) => {
-                if (err) {
-                    console.log("Error while reading json file: ", err);
-                    return;
-                }
-
-                try {
-                    const json = JSON.parse(data);
-
-                    if (json.commands.includes(command)) {
-                        const jsPath = path.join(skillPath, json.runFile);
-                        //const jsPath = path.resolve(skillPath, json.runFile);
-                        
-                        console.log(jsPath);
-
-                        eval(jsPath);
-                        //import(jsPath);
-                        //require(jsPath);
-                    }
-                } catch (err) {
-                    console.log("Error while parsing json file: ", err);
-                    commandFound = false;
-                }
-            });
+function loadSkills() {
+    fs.readdir(skillPath, (err, files) => {
+        if (err) {
+            console.log("Error while reading skillpath: ", err);
+            return;
         }
+
+        files.forEach(file => {
+            if (path.extname(file) === ".json") {
+                const jsonPath = path.join(skillPath, file);
+
+                fs.readFile(jsonPath, "utf8", (err, data) => {
+                    if (err) {
+                        console.log("Error while reading json file: ", err);
+                        return;
+                    }
+
+                    try {
+                        const json = JSON.parse(data);
+
+                        if (json.commands.includes(command)) {
+                            const skillFilePath = skillPath + json.runFile;
+                            const skillModule = require(skillFilePath);
+                            
+                            if(typeof skillModule.onEnable === "function" && typeof skillModule.onDisable === "function") {
+                                skillModule.onEnable();
+                            } else {
+                                console.log("Error: Can't find functions: 'onEnable()' and 'onDisable' in SkillModule: ", skillFilePath)
+                                skillModule.onDisable();
+                            }
+                        }
+                    } catch (err) {
+                        console.log("Error while parsing json file: ", err);
+                    }
+                });
+            }
+        });
     });
-});
+}
+
+
+function unloadSkills() {
+    fs.readdir(skillPath, (err, files) => {
+        if (err) {
+            console.log("Error while reading skillpath: ", err);
+            return;
+        }
+
+        files.forEach(file => {
+            if (path.extname(file) === ".json") {
+                const jsonPath = path.join(skillPath, file);
+
+                fs.readFile(jsonPath, "utf8", (err, data) => {
+                    if (err) {
+                        console.log("Error while reading json file: ", err);
+                        return;
+                    }
+
+                    try {
+                        const json = JSON.parse(data);
+
+                        const skillFilePath = skillPath + json.runFile;
+                        const skillModule = require(skillFilePath);
+                            
+                        skillModule.onDisable();
+                    } catch (err) {
+                        console.log("Error while parsing json file: ", err);
+                    }
+                });
+            }
+        });
+    });
+}
--- skills/test.cjs
+++ skills/test.cjs
@@ -1,1 +1,10 @@
-console.log("Test");
+let onEnable = function() {
+    console.log("onEnable");
+}
+
+let onDisable = function() {
+    console.log("onDisable")
+}
+
+
+module.exports = { onEnable, onDisable };
